31265831c1820c5c761c7b9428fea19f0c195973
taoshiwei <taoshiwei@itcast.cn>
2018-03-22 11:46:40 +0800

    添加 => layout filter lang icon 修复 => 更新 =>

--- src/store/index.js
+++ src/store/index.js
@@ -1,5 +1,9 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import app from '@/src/module-dashboard/store/app'
+import errorLog from '@/src/module-dashboard/store/errorLog'
+import permission from '@/src/module-dashboard/store/permission'
+import tagsView from '@/src/module-dashboard/store/tagsView'
 import user from '@/src/module-dashboard/store/user'
 import getters from './getters'
 
@@ -7,6 +11,10 @@ Vue.use(Vuex)
 
 const store = new Vuex.Store({
   modules: {
+    app,
+    errorLog,
+    permission,
+    tagsView,
     user
   },
   getters
