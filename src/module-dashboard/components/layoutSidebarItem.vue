e77eb531813c5fd8f5ed2951e5bae7f4544cddee
taoshiwei <taoshiwei@itcast.cn>
2018-03-26 13:50:54 +0800

    添加 => 修复 => 导航采用官方标准方案 更新 =>

--- src/module-dashboard/components/layoutSidebarItem.vue
+++ src/module-dashboard/components/layoutSidebarItem.vue
@@ -1,8 +1,41 @@
 <template>
   <div class="menu-wrapper">
+    
+
+    <el-submenu index="1">
+      <template slot="title">
+        <i class="el-icon-location"></i>
+        <span slot="title">导航一</span>
+      </template>
+      <el-menu-item-group>
+        <span slot="title">分组一</span>
+        <el-menu-item index="1-1">选项1</el-menu-item>
+        <el-menu-item index="1-2">选项2</el-menu-item>
+      </el-menu-item-group>
+      <el-menu-item-group title="分组2">
+        <el-menu-item index="1-3">选项3</el-menu-item>
+      </el-menu-item-group>
+      <el-submenu index="1-4">
+        <span slot="title">选项4</span>
+        <el-menu-item index="1-4-1">选项1</el-menu-item>
+      </el-submenu>
+    </el-submenu>
+    <el-menu-item index="2">
+      <i class="el-icon-menu"></i>
+      <span slot="title">导航二</span>
+    </el-menu-item>
+    <el-menu-item index="3" disabled>
+      <i class="el-icon-document"></i>
+      <span slot="title">导航三</span>
+    </el-menu-item>
+    <el-menu-item index="4">
+      <i class="el-icon-setting"></i>
+      <span slot="title">导航四</span>
+    </el-menu-item>
+
     <template v-for="item in routes" v-if="!item.hidden&&item.children">
 
-      <router-link v-if="item.children.length===1 && !item.children[0].children&&!item.alwaysShow" :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">
+      <!-- <router-link v-if="item.children.length===1 && !item.children[0].children&&!item.alwaysShow" :to="item.path+'/'+item.children[0].path" :key="item.children[0].name">
         <el-menu-item :index="item.path+'/'+item.children[0].path" :class="{'submenu-title-noDropdown':!isNest}">
           <svg-icon v-if="item.children[0].meta&&item.children[0].meta.icon" :icon-class="item.children[0].meta.icon"></svg-icon>
           <span v-if="item.children[0].meta&&item.children[0].meta.title" slot="title">{{generateTitle(item.children[0].meta.title)}}</span>
@@ -25,14 +58,14 @@
             </el-menu-item>
           </router-link>
         </template>
-      </el-submenu>
+      </el-submenu> -->
 
     </template>
   </div>
 </template>
 
 <script>
-import { generateTitle } from '@/utils/i18n'
+import {generateTitle} from '@/utils/i18n'
 
 export default {
   name: 'layoutSidebarItem',
