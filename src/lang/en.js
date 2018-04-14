08b751319eaa5ad8d65e3ad9b1b3191c8cd7e9c7
娄江华 <loujianghua@itcast.cn>
2018-04-14 10:01:39 +0800

    add=>

--- src/lang/en.js
+++ src/lang/en.js
@@ -15,13 +15,14 @@ export default {
     theme: 'theme'
   },
   login: {
-    title: 'Login Form',
+    title: 'itheima login',
     logIn: 'Log in',
     username: 'Username',
     password: 'Password',
     any: 'any',
-    thirdparty: 'Or connect with',
-    thirdpartyTips: 'Can not be simulated on local, so please combine you own business simulation! ! !'
+    thirdparty: 'Third',
+    thirdpartyTips:
+      'Can not be simulated on local, so please combine you own business simulation! ! !'
   },
   tagsView: {
     close: 'Close',
@@ -31,15 +32,19 @@ export default {
   table: {
     title: 'Title',
     search: 'Search',
-    addUser: 'add',
+    add: 'add',
+    addUser: 'addUser',
     id: 'ID',
     email: 'Email',
     phone: 'Phone',
     username: 'User',
+    permissionNew: 'permissionNew',
     permissionUser: 'Permission',
     avatar: 'Avatar',
     introduction: 'Introduction',
     paddword: 'paddWord',
+    powerCode: 'Permission code',
+    powerTitle: 'Permission title',
     actions: 'Actions',
     edit: 'Edit',
     delete: 'Delete',
