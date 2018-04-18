73a0daf7bec72b10de49154ec447498d85d2be58
taoshiwei <taoshiwei@itcast.cn>
2018-04-18 10:35:07 +0800

    更新 => 美化侧栏 加入黑马标志

--- src/module-dashboard/components/layoutSidebar.vue
+++ src/module-dashboard/components/layoutSidebar.vue
@@ -1,9 +1,15 @@
 <template>
   <scroll-bar>
-    <el-menu mode="vertical" :default-active="$route.path" :collapse="isCollapse" background-color="#304156" text-color="#bfcbd9" active-text-color="#409EFF">
+    <el-menu mode="vertical" 
+      :default-active="$route.path" 
+      :collapse="isCollapse" 
+      background-color="#001529" 
+      text-color="#bfcbd9" 
+      active-text-color="#fff"
+      >
       <router-link to="/">
-        <div class="sidebar-logo">黑马Admin</div>
-        <div class="sidebar-logo-mini">A</div>
+        <div class="sidebar-logo"><img src="../assets/banner-64.png" /></div>
+        <div class="sidebar-logo-mini"><img src="../assets/logo-64.png" /></div>
       </router-link>
       <sidebar-item :routes="permission_routers"></sidebar-item>
     </el-menu>
--- src/module-list/pages/tableList.vue
+++ src/module-list/pages/tableList.vue
@@ -61,6 +61,15 @@
           <el-table-column prop="reviewer" label="审核" width="80"></el-table-column>
           <el-table-column prop="pageviews" label="浏览" width="80"></el-table-column>
           <el-table-column prop="display_time" label="日期" width="160"></el-table-column>
+          <el-table-column
+            fixed="right"
+            label="操作"
+            width="100">
+            <template slot-scope="scope">
+              <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+              <el-button type="text" size="small">编辑</el-button>
+            </template>
+          </el-table-column>
         </el-table>
         <el-pagination 
           class="pagination"
