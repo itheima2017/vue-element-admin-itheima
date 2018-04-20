1c604600a42cddb52fa2afe8819c7ec8b2606dc0
taoshiwei <taoshiwei@itcast.cn>
2018-04-20 10:47:46 +0800

    添加 => 站内搜索

--- src/module-dashboard/components/layoutNavbar.vue
+++ src/module-dashboard/components/layoutNavbar.vue
@@ -5,53 +5,30 @@
     <breadcrumb class="breadcrumb-container"></breadcrumb>
 
     <div class="right-menu">
-      <error-log class="errLog-container right-menu-item"></error-log>
-
-      <el-tooltip effect="dark" :content="$t('navbar.screenfull')" placement="bottom">
-        <screenfull class="screenfull right-menu-item"></screenfull>
-      </el-tooltip>
-
-      <lang-select class="international right-menu-item"></lang-select>
-
-      <el-tooltip effect="dark" :content="$t('navbar.theme')" placement="bottom">
-        <theme-picker class="theme-switch right-menu-item"></theme-picker>
-      </el-tooltip>
-
-      <el-dropdown class="avatar-container right-menu-item" trigger="click">
-        <div class="avatar-wrapper">
-          <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'">
-          <span class="user-name">{{name}}</span>
-          <!-- <img class="user-avatar" :src="avatar+'?imageView2/1/w/80/h/80'"> -->
-          <i class="el-icon-caret-bottom"></i>
-        </div>
-        <el-dropdown-menu slot="dropdown">
-          <router-link to="/">
-            <el-dropdown-item>
-              {{$t('navbar.dashboard')}}
-            </el-dropdown-item>
-          </router-link>
-          <a target='_blank' href="https://github.com/itheima2017/vue-element-admin-itheima">
-            <el-dropdown-item>
-              {{$t('navbar.github')}}
-            </el-dropdown-item>
-          </a>
-          <el-dropdown-item divided>
-            <span @click="logout" style="display:block;">{{$t('navbar.logOut')}}</span>
-          </el-dropdown-item>
-        </el-dropdown-menu>
-      </el-dropdown>
+      <div class="search-item">
+        <el-button icon="el-icon-search" type="text" class="btnSearch" @click="showSearchInput = !showSearchInput"></el-button>
+        <transition name="el-fade-in-linear">
+          <el-autocomplete 
+            v-model="searchVal" 
+            :fetch-suggestions="querySearchAsync" 
+            @select="handleSelect" 
+            placeholder="站内搜索" 
+            v-show="showSearchInput" ></el-autocomplete>
+        </transition>
+      </div>
     </div>
   </el-menu>
 </template>
 
 <script>
-import { mapGetters } from 'vuex'
+import {mapGetters} from 'vuex'
 import Breadcrumb from '@/components/Breadcrumb'
 import Hamburger from '@/components/Hamburger'
 import ErrorLog from '@/components/ErrorLog'
 import Screenfull from '@/components/Screenfull'
 import LangSelect from '@/components/LangSelect'
 import ThemePicker from '@/components/ThemePicker'
+import {search} from '@/api/base/menus'
 
 export default {
   name: 'layoutNavBar',
@@ -64,11 +41,15 @@ export default {
     ThemePicker
   },
   computed: {
-    ...mapGetters([
-      'sidebar',
-      'name',
-      'avatar'
-    ])
+    ...mapGetters(['sidebar', 'name', 'avatar'])
+  },
+  data() {
+    return {
+      searchVal: '',
+      timeout: null,
+      showSearchInput: false,
+      restaurants: []
+    }
   },
   methods: {
     toggleSideBar() {
@@ -76,12 +57,50 @@ export default {
     },
     logout() {
       this.$store.dispatch('LogOut').then(() => {
-        location.reload()// In order to re-instantiate the vue-router object to avoid bugs
+        location.reload() // In order to re-instantiate the vue-router object to avoid bugs
+      })
+    },
+    querySearchAsync(queryString, cb) {
+      var restaurants = this.restaurants
+      var results = queryString
+        ? restaurants.filter(this.createStateFilter(queryString))
+        : restaurants
+
+      clearTimeout(this.timeout)
+      this.timeout = setTimeout(() => {
+        cb(results)
+      }, 1000 * Math.random())
+    },
+    createStateFilter(queryString) {
+      return state => {
+        return (
+          state.value.toLowerCase().indexOf(queryString.toLowerCase()) === 0
+        )
+      }
+    },
+    handleSelect(item) {
+      this.searchVal = ''
+      this.showSearchInput = false
+      console.log(item)
+      this.$message({
+        message: `选取了 ${item.value}`,
+        type: 'success'
       })
     }
+  },
+  mounted() {
+    this.restaurants = search()
   }
 }
 </script>
+
+<style rel="stylesheet/scss" lang="scss">
+.navbar .search-item .el-input__inner {
+  border: 0px;
+  border-bottom: 1px solid #dcdfe6;
+  border-radius: 0px;
+}
+</style>
 
 <style rel="stylesheet/scss" lang="scss" scoped>
 .navbar {
@@ -94,7 +113,7 @@ export default {
     float: left;
     padding: 0 10px;
   }
-  .breadcrumb-container{
+  .breadcrumb-container {
     float: left;
   }
   .errLog-container {
@@ -103,53 +122,11 @@ export default {
   }
   .right-menu {
     float: right;
-    height: 100%;
-    &:focus{
-     outline: none;
-    }
-    .right-menu-item {
-      display: inline-block;
-      margin: 0 8px;
-    }
-    .screenfull {
-      margin-top: 15px;
-      height: 20px;
-    }
-    .international{
-      vertical-align: top;
-    }
-    .theme-switch {
-      vertical-align: 15px;
-    }
-    .avatar-container {
-      height: 50px;
-      margin-right: 30px;
-      .avatar-wrapper {
-        cursor: pointer;
-        margin-top: 5px;
-        position: relative;
-        width: 100px;
-        .user-avatar {
-          width: 40px;
-          height: 40px;
-          border-radius: 10px;
-        }
-        .user-name {
-          position:absolute;
-          top: -5px;
-          left: 45px;
-          right: 0px;
-          width: 60px;
-          height: 50px;
-          overflow: hidden;
-        }
-        .el-icon-caret-bottom {
-          position: absolute;
-          right: -20px;
-          top: 15px;
-          font-size: 12px;
-        }
-      }
+    height: 50px;
+    margin-right: 20px;
+    .search-item > .btnSearch {
+      font-size: 16px;
+      color: rgba(0, 0, 0, 0.65);
     }
   }
 }
