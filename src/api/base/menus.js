e814ab9a474d3110d53494892732a916cf633bb7
娄江华 <loujianghua@itcast.cn>
2018-05-03 14:39:09 +0800

    update=>

--- src/api/base/menus.js
+++ src/api/base/menus.js
@@ -1,7 +1,7 @@
-import {createAPI} from '@/utils/request'
+import { createAPI } from '@/utils/request'
 
 export const list = data => createAPI('/base/menus', 'get', data)
-export const add = data => createAPI('/base/users', 'post', data)
-export const update = data => createAPI('/base/users', 'put', data)
-export const remove = data => createAPI('/base/users', 'delete', data)
-export const detail = data => createAPI('/base/users/:id', 'get', data)
+export const add = data => createAPI('/base/menus', 'post', data)
+export const update = data => createAPI('/base/menus/', 'put', data)
+export const remove = data => createAPI('/base/menus/', 'delete', data)
+export const detail = data => createAPI('/base/menus/:id', 'get', data)
