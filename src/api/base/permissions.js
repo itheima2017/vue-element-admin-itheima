a2b4936d0b99cc3327cf60923fdfe23352a7dd4f
taoshiwei <taoshiwei@itcast.cn>
2018-05-08 17:27:19 +0800

    修复 => 接口 :id 修复

--- src/api/base/permissions.js
+++ src/api/base/permissions.js
@@ -3,6 +3,6 @@ import {createAPI} from '@/utils/request'
 export const list = data => createAPI('/base/permissions', 'get', data)
 export const simple = data => createAPI('/base/permissions/simple', 'get', data)
 export const add = data => createAPI('/base/permissions', 'post', data)
-export const update = data => createAPI('/base/permissions/:id', 'put', data)
+export const update = data => createAPI(`/base/permissions/${data.id}`, 'put', data)
 export const remove = data => createAPI(`/base/permissions/${data.id}`, 'delete', data)
 export const detail = data => createAPI(`/base/permissions/${data.id}`, 'get', data)
--- src/api/base/users.js
+++ src/api/base/users.js
@@ -3,6 +3,6 @@ import {createAPI} from '@/utils/request'
 export const list = data => createAPI('/base/users/', 'get', data)
 export const simple = data => createAPI('/base/users/simple', 'get', data)
 export const add = data => createAPI('/base/users', 'post', data)
-export const update = data => createAPI('/base/users/:id', 'put', data)
+export const update = data => createAPI(`/base/users/${data.id}`, 'put', data)
 export const remove = data => createAPI(`/base/users/${data.id}`, 'delete', data)
 export const detail = data => createAPI(`/base/users/${data.id}`, 'get', data)
