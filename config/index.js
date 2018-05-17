5b1b20c1a3159dadc1b8d79b5539cd3ac2f09e9f
taoshiwei <taoshiwei@itcast.cn>
2018-05-17 13:58:07 +0800

    更新 => 资源相对地址

--- config/index.js
+++ config/index.js
@@ -8,7 +8,7 @@ module.exports = {
   dev: {
     // Paths
     assetsSubDirectory: 'static',
-    assetsPublicPath: '/',
+    assetsPublicPath: '',
     proxyTable: {
       '/api': {
         // target: 'https://www.easy-mock.com/mock/5ab213e33666166110a94928/admin',
