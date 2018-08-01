7cab460ac8fd74c387658fdd2211a9013ea22611
陶峙巍 <taoshiwei@itcast.cn>
2018-08-01 14:52:36 +0800

    修复 => 大小写 多个单词用 - 链接

--- src/module-form/components/address.vue
+++ src/module-form/components/address.vue
@@ -45,7 +45,7 @@
 </el-form>
 </template>
 <script>
-import addressData from './AddressData.js'
+import addressData from './address-data.js'
 import store from '../store/store.js'
 function formatData(data) {
   var result = []
