710f95feb4142d3e2c0f651f32d4ac5ecbfd9642
陶峙巍 <taoshiwei@itcast.cn>
2018-08-02 14:42:47 +0800

    添加 => only_issue_from_commit_file

--- ci/sonar_preview.sh
+++ ci/sonar_preview.sh
@@ -7,7 +7,8 @@ sonar-scanner \
   -Dsonar.projectVersion=1.0.$CI_PIPELINE_ID \
   -Dsonar.gitlab.project_id=$CI_PROJECT_ID \
   -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA \
-  -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME 
+  -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME \
+  -Dsonar.gitlab.only_issue_from_commit_file=false
 
 if [ $? -eq 0 ]; then
     echo "sonarqube code-publish over."
