18448ce49cb6c06cf252d3110a7fe0403e48e236
陶峙巍 <taoshiwei@itcast.cn>
2018-08-02 16:18:40 +0800

    添加 => commit_sha

--- ci/sonar_preview.sh
+++ ci/sonar_preview.sh
@@ -5,14 +5,14 @@ sonar-scanner -X \
   -Dsonar.projectKey=gitlab:$CI_COMMIT_REF_NAME:$CI_PROJECT_NAME \
   -Dsonar.projectName=gitlab:$CI_COMMIT_REF_NAME:$CI_PROJECT_NAME \
   -Dsonar.projectVersion=1.0.$CI_PIPELINE_ID \
+  -Dsonar.issuesReport.html.enable=true \
   -Dsonar.gitlab.project_id=$CI_PROJECT_ID \
   -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA \
-  -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME \
-  -Dsonar.gitlab.ignore_certificate=true \
-  -Dsonar.gitlab.disable_global_comment=true \
-  -Dsonar.gitlab.unique_issue_per_inline=true
+  -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME
 
-
+  # -Dsonar.gitlab.ignore_certificate=true \
+  # -Dsonar.gitlab.disable_global_comment=true \
+  # -Dsonar.gitlab.unique_issue_per_inline=true
   # -Dsonar.gitlab.failure_notification_mode=exit-code
   # -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA \
   # -Dsonar.gitlab.disable_global_comment=true \
