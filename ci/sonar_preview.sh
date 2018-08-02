6fdc0f0c5586ff76261b249af6c2ef283e3b4af4
陶峙巍 <taoshiwei@itcast.cn>
2018-08-02 15:24:30 +0800

    添加 => CI_COMMIT_SHA

--- ci/sonar_preview.sh
+++ ci/sonar_preview.sh
@@ -6,7 +6,7 @@ sonar-scanner -X \
   -Dsonar.projectName=gitlab:$CI_COMMIT_REF_NAME:$CI_PROJECT_NAME \
   -Dsonar.projectVersion=1.0.$CI_PIPELINE_ID \
   -Dsonar.gitlab.project_id=$CI_PROJECT_ID \
-  -Dsonar.gitlab.commit_sha=$CI_COMMIT_REF \
+  -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA \
   -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME
 
   # -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA \
