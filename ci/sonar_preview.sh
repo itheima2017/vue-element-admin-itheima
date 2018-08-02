115cf83cdc4395765c12b828a0d7cc1faaf168b7
陶峙巍 <taoshiwei@itcast.cn>
2018-08-02 14:52:10 +0800

    添加 => only_issue_from_commit_file

--- ci/sonar_preview.sh
+++ ci/sonar_preview.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sonar-scanner \
+sonar-scanner -X \
   -Dsonar.analysis.mode=preview \
   -Dsonar.projectKey=gitlab:$CI_COMMIT_REF_NAME:$CI_PROJECT_NAME \
   -Dsonar.projectName=gitlab:$CI_COMMIT_REF_NAME:$CI_PROJECT_NAME \
@@ -8,7 +8,7 @@ sonar-scanner \
   -Dsonar.gitlab.project_id=$CI_PROJECT_ID \
   -Dsonar.gitlab.commit_sha=$CI_COMMIT_SHA \
   -Dsonar.gitlab.ref_name=$CI_COMMIT_REF_NAME \
-  -Dsonar.gitlab.failure_notification_mode=exit-code
+  -Dsonar.gitlab.failure_notification_mode=exit-code \
   -Dsonar.gitlab.only_issue_from_commit_file=false
 
 if [ $? -eq 0 ]; then
