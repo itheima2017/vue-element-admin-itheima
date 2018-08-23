178d0989d8db8bf5d099f7a7fb27950eed2f384d
娄江华 <loujianghua@itcast.cn>
2018-08-23 15:10:51 +0800

    update=>

--- src/module-manage/components/permissions-add.vue
+++ src/module-manage/components/permissions-add.vue
@@ -156,7 +156,7 @@ export default {
       let curPermissions = []
       let nodesPath = []
       function parseNodes(nodes, findId) {
-        for (let it of nodes) {
+        nodes.map(function(it, index) {
           nodesPath.push(it)
           let isFind = false
           if (findId === it.id) {
@@ -173,17 +173,19 @@ export default {
           } else {
           }
           if (isFind) {
-            for (let item of nodesPath) {
-              if (curPermissions.indexOf(item.id) === -1) {
-                curPermissions.push(findId)
-              }
-            }
+            nodeDate()
             nodesPath = []
             return isFind
           }
+          function nodeDate() {
+            nodesPath.map(function(item, index) {
+              if (curPermissions.indexOf(item.id) === -1) {
+                curPermissions.push(findId)
+              }
+            })
+          }
           nodesPath.pop()
-          // return isFind
-        }
+        })
       }
       // console.log(this.$refs.treeMenu.getCheckedNodes())
       if (this.treeCheckedNodes.length === 0) {
